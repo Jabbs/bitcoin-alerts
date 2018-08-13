b852f21f0c573d59766f2d2463724934efa2d843
Pete Jabbour <petejabbour1@gmail.com>
2018-08-13 09:47:13 -0500

    twit stuff

--- app/models/twitter_service.rb
+++ app/models/twitter_service.rb
@@ -10,4 +10,32 @@ class TwitterService < ActiveRecord::Base
     end
     @client
   end
+
+  def self.find_tweets(user="CoinDesk", keywords=["Bitcoin", "up"])
+    client = TwitterService.client
+    x = []
+    tweets = client.user_timeline(user, {count: 200})
+    x += TwitterService.tweets_with_keyword(tweets, keywords)
+    1000.times do
+      if tweets.count > 1
+        max_id = tweets.last.try(:id)
+      elsif max_id.present?
+        max_id = max_id - 994251203876933633
+      end
+      next unless max_id.present?
+      tweets = client.user_timeline(user, {count: 200, max_id: max_id})
+      x += TwitterService.tweets_with_keyword(tweets, keywords)
+    end
+    x
+  end
+
+  def self.tweets_with_keyword(tweets, keywords)
+    x = []
+    tweets.each do |tweet|
+      add_tweet = false
+      add_tweet = keywords.all? { |k| tweet.text.include?(k) }
+      x << tweet if add_tweet
+    end
+    x
+  end
 end
